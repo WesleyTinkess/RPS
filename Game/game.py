5f49efb2d02adb266767342bd6d3dbeecb16d937
Nylvia <83308702+nylvia1@users.noreply.github.com>
2025-11-13 00:26:05 +0000

    Adds computer choice

--- Game/game.py
+++ Game/game.py
@@ -1,1 +1,7 @@
 print("Welcome to Rock-Paper-Scissors!")
+
+
+import random
+options = {1:"Rock", 2:"Paper", 3:"Scissors"}
+computer_choice = random.randint(1,3)
+print(f"Computer chose {options[computer_choice]}")
